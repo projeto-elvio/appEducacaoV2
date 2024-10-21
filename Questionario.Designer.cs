df8e022ac2266371bd9a4b802e353644f9cf4965
Otavio Aparecido Pegorin <24110300@labcomp.usc>
2024-10-21 19:40:33 -0300

    Adicionando questionario

---
+++ Questionario.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace appEducacao
+{
+    partial class Questionario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Questionario));
+            btnEnviar = new Button();
+            lblPergunta = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            pictureBox7 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            lnkSaude = new LinkLabel();
+            lnkEsportes = new LinkLabel();
+            lnkEducacao = new LinkLabel();
+            lnkLiberdade = new LinkLabel();
+            lnkCultura = new LinkLabel();
+            lnkFamilia = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            lblTituloQuestionario = new Label();
+            lblNumero = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            btnEnviar.BackColor = Color.FromArgb(192, 192, 255);
+            btnEnviar.FlatStyle = FlatStyle.Flat;
+            btnEnviar.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEnviar.ForeColor = SystemColors.InfoText;
+            btnEnviar.Location = new Point(628, 372);
+            btnEnviar.Name = "btnEnviar";
+            btnEnviar.Size = new Size(129, 48);
+            btnEnviar.TabIndex = 0;
+            btnEnviar.Text = "Enviar";
+            btnEnviar.UseVisualStyleBackColor = false;
+            btnEnviar.Click += btnEnviar_Click;
+            // 
+            // lblPergunta
+            // 
+            lblPergunta.Font = new Font("Trajan Pro", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPergunta.Location = new Point(258, 130);
+            lblPergunta.Name = "lblPergunta";
+            lblPergunta.Size = new Size(513, 103);
+            lblPergunta.TabIndex = 1;
+            lblPergunta.Text = "Pergunta";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(245, 249);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(88, 19);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "rbtnOpcao1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(245, 289);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(88, 19);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "rbtnOpcao2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(245, 329);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(88, 19);
+            radioButton3.TabIndex = 4;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "rbtnOpcao3";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(245, 368);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(88, 19);
+            radioButton4.TabIndex = 5;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "rbtnOpcao4";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(50, 357);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(30, 30);
+            pictureBox7.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox7.TabIndex = 66;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(50, 294);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(30, 30);
+            pictureBox6.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox6.TabIndex = 65;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(50, 236);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(30, 30);
+            pictureBox5.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox5.TabIndex = 64;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(50, 170);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(30, 30);
+            pictureBox4.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox4.TabIndex = 63;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(50, 100);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(30, 30);
+            pictureBox3.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox3.TabIndex = 62;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(50, 34);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(30, 30);
+            pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox2.TabIndex = 61;
+            pictureBox2.TabStop = false;
+            // 
+            // lnkSaude
+            // 
+            lnkSaude.AutoSize = true;
+            lnkSaude.BackColor = Color.FromArgb(192, 192, 255);
+            lnkSaude.LinkBehavior = LinkBehavior.NeverUnderline;
+            lnkSaude.LinkColor = Color.Black;
+            lnkSaude.Location = new Point(86, 372);
+            lnkSaude.Name = "lnkSaude";
+            lnkSaude.Size = new Size(39, 15);
+            lnkSaude.TabIndex = 60;
+            lnkSaude.TabStop = true;
+            lnkSaude.Text = "Saúde";
+            // 
+            // lnkEsportes
+            // 
+            lnkEsportes.AutoSize = true;
+            lnkEsportes.BackColor = Color.FromArgb(192, 192, 255);
+            lnkEsportes.LinkBehavior = LinkBehavior.NeverUnderline;
+            lnkEsportes.LinkColor = Color.Black;
+            lnkEsportes.Location = new Point(86, 309);
+            lnkEsportes.Name = "lnkEsportes";
+            lnkEsportes.Size = new Size(51, 15);
+            lnkEsportes.TabIndex = 59;
+            lnkEsportes.TabStop = true;
+            lnkEsportes.Text = "Esportes";
+            // 
+            // lnkEducacao
+            // 
+            lnkEducacao.AutoSize = true;
+            lnkEducacao.BackColor = Color.FromArgb(192, 192, 255);
+            lnkEducacao.LinkBehavior = LinkBehavior.NeverUnderline;
+            lnkEducacao.LinkColor = Color.Black;
+            lnkEducacao.Location = new Point(86, 251);
+            lnkEducacao.Name = "lnkEducacao";
+            lnkEducacao.Size = new Size(58, 15);
+            lnkEducacao.TabIndex = 58;
+            lnkEducacao.TabStop = true;
+            lnkEducacao.Text = "Educação";
+            // 
+            // lnkLiberdade
+            // 
+            lnkLiberdade.AutoSize = true;
+            lnkLiberdade.BackColor = Color.FromArgb(192, 192, 255);
+            lnkLiberdade.LinkBehavior = LinkBehavior.NeverUnderline;
+            lnkLiberdade.LinkColor = Color.Black;
+            lnkLiberdade.Location = new Point(86, 185);
+            lnkLiberdade.Name = "lnkLiberdade";
+            lnkLiberdade.Size = new Size(59, 15);
+            lnkLiberdade.TabIndex = 57;
+            lnkLiberdade.TabStop = true;
+            lnkLiberdade.Text = "Liberdade";
+            // 
+            // lnkCultura
+            // 
+            lnkCultura.AutoSize = true;
+            lnkCultura.BackColor = Color.FromArgb(192, 192, 255);
+            lnkCultura.LinkBehavior = LinkBehavior.NeverUnderline;
+            lnkCultura.LinkColor = Color.Black;
+            lnkCultura.Location = new Point(86, 115);
+            lnkCultura.Name = "lnkCultura";
+            lnkCultura.Size = new Size(46, 15);
+            lnkCultura.TabIndex = 56;
+            lnkCultura.TabStop = true;
+            lnkCultura.Text = "Cultura";
+            // 
+            // lnkFamilia
+            // 
+            lnkFamilia.AutoSize = true;
+            lnkFamilia.BackColor = Color.FromArgb(192, 192, 255);
+            lnkFamilia.DisabledLinkColor = Color.Black;
+            lnkFamilia.LinkBehavior = LinkBehavior.NeverUnderline;
+            lnkFamilia.LinkColor = Color.Black;
+            lnkFamilia.Location = new Point(86, 49);
+            lnkFamilia.Name = "lnkFamilia";
+            lnkFamilia.Size = new Size(45, 15);
+            lnkFamilia.TabIndex = 55;
+            lnkFamilia.TabStop = true;
+            lnkFamilia.Text = "Família";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(193, 453);
+            pictureBox1.TabIndex = 54;
+            pictureBox1.TabStop = false;
+            // 
+            // lblTituloQuestionario
+            // 
+            lblTituloQuestionario.Font = new Font("Segoe UI", 35F);
+            lblTituloQuestionario.Location = new Point(235, -5);
+            lblTituloQuestionario.Name = "lblTituloQuestionario";
+            lblTituloQuestionario.Size = new Size(536, 135);
+            lblTituloQuestionario.TabIndex = 67;
+            lblTituloQuestionario.Text = "Questionário";
+            lblTituloQuestionario.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblNumero
+            // 
+            lblNumero.Font = new Font("Trajan Pro", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblNumero.Location = new Point(212, 130);
+            lblNumero.Name = "lblNumero";
+            lblNumero.Size = new Size(56, 27);
+            lblNumero.TabIndex = 68;
+            lblNumero.Text = "00 -";
+            // 
+            // Questionario
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblNumero);
+            Controls.Add(lblTituloQuestionario);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(lnkSaude);
+            Controls.Add(lnkEsportes);
+            Controls.Add(lnkEducacao);
+            Controls.Add(lnkLiberdade);
+            Controls.Add(lnkCultura);
+            Controls.Add(lnkFamilia);
+            Controls.Add(pictureBox1);
+            Controls.Add(radioButton4);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(lblPergunta);
+            Controls.Add(btnEnviar);
+            Name = "Questionario";
+            Text = "Questionario";
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnEnviar;
+        private Label lblPergunta;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private LinkLabel lnkSaude;
+        private LinkLabel lnkEsportes;
+        private LinkLabel lnkEducacao;
+        private LinkLabel lnkLiberdade;
+        private LinkLabel lnkCultura;
+        private LinkLabel lnkFamilia;
+        private PictureBox pictureBox1;
+        private Label lblTituloQuestionario;
+        private Label lblNumero;
+    }
+}
---
+++ Questionario.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appEducacao
+{
+    public partial class Questionario : Form
+    {
+        // Lista de perguntas
+        private List<Pergunta> perguntas;
+        private int perguntaAtual = 0;
+
+        public Questionario()
+        {
+            InitializeComponent();
+            CarregarPerguntas();
+            CarregarPergunta();
+        }
+
+        private void CarregarPerguntas()
+        {
+            // Adicionando perguntas
+            perguntas = new List<Pergunta>
+        {
+            new Pergunta { Texto = "Quando foi promulgado o Estatuto da Criança e do Adolescente (ECA)?", Opcoes = new[] { "1988", "1990", "1989", "1992" }, RespostaCorreta = 1 },
+            new Pergunta { Texto = "Qual documento internacional assinado pelo Brasil em 1989 trata da proteção dos direitos das crianças e adolescentes?", Opcoes = new[] { "Declaração Universal dos Direitos Humanos", "Pacto de São José da Costa Rica", "Convenção sobre os Direitos da Criança (ONU)", "Protocolo Facultativo sobre a Criança" }, RespostaCorreta = 2 },
+            new Pergunta { Texto = "Segundo a Constituição Federal de 1988, quem é responsável por assegurar os direitos das crianças e adolescentes?", Opcoes = new[] { "Apenas o Estado", "Apenas a família", "A família, a sociedade e o Estado", "ONGs e associações" }, RespostaCorreta = 2 },
+            new Pergunta { Texto = "Qual dos seguintes direitos NÃO está incluído nos direitos fundamentais garantidos pelo ECA?", Opcoes = new[] { "Direito à vida", "Direito ao trabalho infantil", "Direito à saúde", "Direito à convivência familiar" }, RespostaCorreta = 1 },
+            new Pergunta { Texto = "Qual órgão tem como função zelar pelo cumprimento dos direitos das crianças e adolescentes e aplicar medidas de proteção em caso de violação?", Opcoes = new[] { "Ministério da Justiça", "Polícia Federal", "Secretaria de Direitos Humanos", "Conselho Tutelar" }, RespostaCorreta = 3 },
+            new Pergunta { Texto = "Qual é o papel da família, segundo o ECA, na proteção das crianças e adolescentes?", Opcoes = new[] { "Educar e cuidar, sendo o primeiro núcleo de proteção", "Apenas fornecer sustento financeiro", "Deixar a proteção a cargo do Estado", "Supervisionar apenas a educação formal" }, RespostaCorreta = 0 },
+            new Pergunta { Texto = "Qual é um dos principais desafios que ainda persiste, mesmo com os avanços na proteção às crianças e adolescentes?", Opcoes = new[] { "Educação universal", "Políticas públicas para esportes", "Violência doméstica e exploração sexual", "Acesso à cultura" }, RespostaCorreta = 2 },
+            new Pergunta { Texto = "Quais medidas são essenciais para garantir que as crianças e adolescentes em situação de vulnerabilidade sejam protegidas?", Opcoes = new[] { "Políticas públicas de inclusão social", "Apenas ajuda financeira", "Redução de impostos", "Aumento de campanhas publicitárias" }, RespostaCorreta = 0 },
+            new Pergunta { Texto = "Quem atua na fiscalização do cumprimento da legislação e na responsabilização em casos de violação dos direitos das crianças e adolescentes?", Opcoes = new[] { "Conselhos Tutelares", "ONG's", "Ministério Público e Poder Judiciário", "Empresas privadas" }, RespostaCorreta = 2 },
+            new Pergunta { Texto = "Para garantir o pleno desenvolvimento das crianças e adolescentes, o que é necessário da parte da sociedade?", Opcoes = new[] { "Indiferença aos problemas sociais", "Esperar que o Estado resolva tudo", "Redução da atuação de movimentos sociais", "Consciência e engajamento coletivo na defesa dos direitos" }, RespostaCorreta = 3 },
+        };
+        }
+
+        private void CarregarPergunta()
+        {
+            var pergunta = perguntas[perguntaAtual];
+            var num = (perguntaAtual + 1) + "-";
+            lblPergunta.Text = pergunta.Texto;
+            lblNumero.Text = num;
+            radioButton1.Text = pergunta.Opcoes[0];
+            radioButton2.Text = pergunta.Opcoes[1];
+            radioButton3.Text = pergunta.Opcoes[2];
+            radioButton4.Text = pergunta.Opcoes[3];
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            int respostaSelecionada = -1;
+
+            if (radioButton1.Checked) respostaSelecionada = 0;
+            else if (radioButton2.Checked) respostaSelecionada = 1;
+            else if (radioButton3.Checked) respostaSelecionada = 2;
+            else if (radioButton4.Checked) respostaSelecionada = 3;
+
+            if (respostaSelecionada == perguntas[perguntaAtual].RespostaCorreta)
+            {
+                MessageBox.Show("Resposta correta!");
+            }
+            else
+            {
+                MessageBox.Show("Resposta incorreta!");
+            }
+
+            perguntaAtual++;
+            if (perguntaAtual < perguntas.Count)
+            {
+                CarregarPergunta();
+            }
+            else
+            {
+                MessageBox.Show("Teste concluído!");
+            }
+
+        }
+
+
+    }
+}
+
+// Classe para perguntas
+public class Pergunta
+{
+    public string Texto { get; set; }
+    public string[] Opcoes { get; set; }
+    public int RespostaCorreta { get; set; }
+}
