d1089da4ed4de57207a5333500ee21b1422aeafe
Leonardo Lopes <151799593+LeoStarkBR@users.noreply.github.com>
2024-10-26 17:45:11 -0300

    Add files via upload
    Feito a pagina sobre cultura

---
+++ Cultura.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace appEducacao
+{
+    partial class Cultura
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+     
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cultura));
+            lblAcessoEducacao = new Label();
+            pictureBox8 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            lnkSaude = new LinkLabel();
+            lnkEsportes = new LinkLabel();
+            lnkEducacao = new LinkLabel();
+            lnkLiberdade = new LinkLabel();
+            lnkCultura = new LinkLabel();
+            lnkFamilia = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // lblAcessoEducacao
+            // 
+            lblAcessoEducacao.AutoSize = true;
+            lblAcessoEducacao.Font = new Font("Segoe UI", 35F);
+            lblAcessoEducacao.Location = new Point(374, 9);
+            lblAcessoEducacao.Name = "lblAcessoEducacao";
+            lblAcessoEducacao.Size = new Size(222, 62);
+            lblAcessoEducacao.TabIndex = 37;
+            lblAcessoEducacao.Text = "CULTURA";
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(199, 158);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(226, 199);
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex = 35;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(50, 357);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(30, 30);
+            pictureBox7.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox7.TabIndex = 34;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(50, 294);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(30, 30);
+            pictureBox6.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox6.TabIndex = 33;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(50, 236);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(30, 30);
+            pictureBox5.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox5.TabIndex = 32;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(50, 170);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(30, 30);
+            pictureBox4.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox4.TabIndex = 31;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(50, 100);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(30, 30);
+            pictureBox3.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox3.TabIndex = 30;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(50, 34);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(30, 30);
+            pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox2.TabIndex = 29;
+            pictureBox2.TabStop = false;
+            // 
+            // lnkSaude
+            // 
+            lnkSaude.AutoSize = true;
+            lnkSaude.BackColor = Color.FromArgb(192, 192, 255);
+            lnkSaude.LinkBehavior = LinkBehavior.NeverUnderline;
+            lnkSaude.LinkColor = Color.Black;
+            lnkSaude.Location = new Point(86, 372);
+            lnkSaude.Name = "lnkSaude";
+            lnkSaude.Size = new Size(39, 15);
+            lnkSaude.TabIndex = 28;
+            lnkSaude.TabStop = true;
+            lnkSaude.Text = "Saúde";
+            // 
+            // lnkEsportes
+            // 
+            lnkEsportes.AutoSize = true;
+            lnkEsportes.BackColor = Color.FromArgb(192, 192, 255);
+            lnkEsportes.LinkBehavior = LinkBehavior.NeverUnderline;
+            lnkEsportes.LinkColor = Color.Black;
+            lnkEsportes.Location = new Point(86, 309);
+            lnkEsportes.Name = "lnkEsportes";
+            lnkEsportes.Size = new Size(51, 15);
+            lnkEsportes.TabIndex = 27;
+            lnkEsportes.TabStop = true;
+            lnkEsportes.Text = "Esportes";
+            // 
+            // lnkEducacao
+            // 
+            lnkEducacao.AutoSize = true;
+            lnkEducacao.BackColor = Color.FromArgb(192, 192, 255);
+            lnkEducacao.LinkBehavior = LinkBehavior.NeverUnderline;
+            lnkEducacao.LinkColor = Color.Black;
+            lnkEducacao.Location = new Point(86, 251);
+            lnkEducacao.Name = "lnkEducacao";
+            lnkEducacao.Size = new Size(58, 15);
+            lnkEducacao.TabIndex = 26;
+            lnkEducacao.TabStop = true;
+            lnkEducacao.Text = "Educação";
+            // 
+            // lnkLiberdade
+            // 
+            lnkLiberdade.AutoSize = true;
+            lnkLiberdade.BackColor = Color.FromArgb(192, 192, 255);
+            lnkLiberdade.LinkBehavior = LinkBehavior.NeverUnderline;
+            lnkLiberdade.LinkColor = Color.Black;
+            lnkLiberdade.Location = new Point(86, 185);
+            lnkLiberdade.Name = "lnkLiberdade";
+            lnkLiberdade.Size = new Size(59, 15);
+            lnkLiberdade.TabIndex = 25;
+            lnkLiberdade.TabStop = true;
+            lnkLiberdade.Text = "Liberdade";
+            // 
+            // lnkCultura
+            // 
+            lnkCultura.AutoSize = true;
+            lnkCultura.BackColor = Color.FromArgb(192, 192, 255);
+            lnkCultura.LinkBehavior = LinkBehavior.NeverUnderline;
+            lnkCultura.LinkColor = Color.Black;
+            lnkCultura.Location = new Point(86, 115);
+            lnkCultura.Name = "lnkCultura";
+            lnkCultura.Size = new Size(46, 15);
+            lnkCultura.TabIndex = 24;
+            lnkCultura.TabStop = true;
+            lnkCultura.Text = "Cultura";
+            // 
+            // lnkFamilia
+            // 
+            lnkFamilia.AutoSize = true;
+            lnkFamilia.BackColor = Color.FromArgb(192, 192, 255);
+            lnkFamilia.DisabledLinkColor = Color.Black;
+            lnkFamilia.LinkBehavior = LinkBehavior.NeverUnderline;
+            lnkFamilia.LinkColor = Color.Black;
+            lnkFamilia.Location = new Point(86, 49);
+            lnkFamilia.Name = "lnkFamilia";
+            lnkFamilia.Size = new Size(45, 15);
+            lnkFamilia.TabIndex = 23;
+            lnkFamilia.TabStop = true;
+            lnkFamilia.Text = "Família";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(192, 192, 255);
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(193, 453);
+            pictureBox1.TabIndex = 22;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(199, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(567, 84);
+            label1.TabIndex = 38;
+            label1.Text = resources.GetString("label1.Text");
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(431, 140);
+            label2.Name = "label2";
+            label2.Size = new Size(343, 60);
+            label2.TabIndex = 39;
+            label2.Text = "Artigo 4º\r\nAfirma que é dever da família, da sociedade e do poder público \r\nassegurar os direitos fundamentais de crianças e adolescentes, \r\nincluindo o acesso à cultura, lazer e esportes.";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(431, 210);
+            label3.Name = "label3";
+            label3.Size = new Size(359, 75);
+            label3.TabIndex = 40;
+            label3.Text = resources.GetString("label3.Text");
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(431, 294);
+            label4.Name = "label4";
+            label4.Size = new Size(373, 60);
+            label4.TabIndex = 41;
+            label4.Text = resources.GetString("label4.Text");
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(230, 368);
+            label5.Name = "label5";
+            label5.Size = new Size(544, 60);
+            label5.TabIndex = 42;
+            label5.Text = resources.GetString("label5.Text");
+            // 
+            // Familia
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Lavender;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(lblAcessoEducacao);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(lnkSaude);
+            Controls.Add(lnkEsportes);
+            Controls.Add(lnkEducacao);
+            Controls.Add(lnkLiberdade);
+            Controls.Add(lnkCultura);
+            Controls.Add(lnkFamilia);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox8);
+            Name = "Familia";
+            Text = "Familia";
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblAcessoEducacao;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private LinkLabel lnkSaude;
+        private LinkLabel lnkEsportes;
+        private LinkLabel lnkEducacao;
+        private LinkLabel lnkLiberdade;
+        private LinkLabel lnkCultura;
+        private LinkLabel lnkFamilia;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+    }
+}
