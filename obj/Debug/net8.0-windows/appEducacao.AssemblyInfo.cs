3029350cb8ab421498fd0d048d41ad40cd8c93a6
ThaisHTML <thais.carolina.henrique@gmail.com>
2024-11-26 00:55:35 -0300

    commit
    25/11/2024

--- obj/Debug/net8.0-windows/appEducacao.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/appEducacao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appEducacao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c13b5c992ca20f85fe95f8c45599f585d068cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c53a3a09909fcd28f07bd891e82dcaceeabf857")]
 [assembly: System.Reflection.AssemblyProductAttribute("appEducacao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appEducacao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
